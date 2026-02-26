6c7110686d75815cc7145720e6ea07425bc2fea6
Anish Agrawal <aniani340340@gmail.com>
2026-02-27 00:00:02 +0530

    UC9: Recursive palindrome checker

---
+++ CHECKER APP/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class UseCase9PalindromeCheckerApp {
+
+    // Recursive method
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don't match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine().replaceAll("\\s+", "").toLowerCase();
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result) {
+            System.out.println("The word \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + input + "\" is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
